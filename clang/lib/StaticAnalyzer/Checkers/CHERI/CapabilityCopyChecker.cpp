36d391f2d1a4b1e511056d54bac3d950f6f1f5e6
Irina Dudina <irina.dudina@ed.ac.uk>
2023-07-26 21:02:07 +0100

    [CHERI_CSA] CHERIUtils

--- clang/lib/StaticAnalyzer/Checkers/CHERI/CapabilityCopyChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/CHERI/CapabilityCopyChecker.cpp
@@ -116,11 +116,6 @@ const MemRegion *stripNonCapShift(const MemRegion *R, ASTContext &ASTCtx) {
   return ER->getSuperRegion();
 }
 
-bool isGenericPointerType(const QualType T) {
-  return T->isVoidPointerType() ||
-         (T->isPointerType() && T->getPointeeType()->isCharType());
-}
-
 bool isVoidOrCharPtrArgRegion(const MemRegion *Reg) {
   if (!Reg)
     return false;
@@ -170,14 +165,6 @@ CHERITagState getTagState(SVal Val, CheckerContext &C,
   }
 
   return CHERITagState::getUnknown();
-}
-
-CharUnits getCapabilityTypeSize(ASTContext &ASTCtx) {
-  return ASTCtx.getTypeSizeInChars(ASTCtx.VoidPtrTy);
-}
-
-CharUnits getCapabilityTypeAlign(ASTContext &ASTCtx) {
-  return ASTCtx.getTypeAlignInChars(ASTCtx.VoidPtrTy);
 }
 
 bool isCapabilityStorage(CheckerContext &C, const MemRegion *R,

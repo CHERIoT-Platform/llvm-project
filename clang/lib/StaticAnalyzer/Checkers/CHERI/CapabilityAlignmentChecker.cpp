43da07c2a700d96662e517263a008df7d656feb7
Irina Dudina <irina.dudina@ed.ac.uk>
2023-08-22 19:03:14 +0100

    [CHERI_CSA] CapabilityAlignmentChecker: removing dead symbols

--- clang/lib/StaticAnalyzer/Checkers/CHERI/CapabilityAlignmentChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/CHERI/CapabilityAlignmentChecker.cpp
@@ -43,7 +43,7 @@ using namespace cheri;
 namespace {
 class CapabilityAlignmentChecker
     : public Checker<check::PreStmt<CastExpr>, check::PostStmt<CastExpr>,
-                     check::PostStmt<BinaryOperator>> {
+                     check::PostStmt<BinaryOperator>, check::DeadSymbols> {
   std::unique_ptr<BugType> CastAlignBug;
   std::unique_ptr<BugType> CapCastAlignBug;
 
@@ -53,6 +53,7 @@ public:
   void checkPostStmt(const BinaryOperator *BO, CheckerContext &C) const;
   void checkPostStmt(const CastExpr *BO, CheckerContext &C) const;
   void checkPreStmt(const CastExpr *BO, CheckerContext &C) const;
+  void checkDeadSymbols(SymbolReaper &SymReaper, CheckerContext &C) const;
 
 private:
   ExplodedNode *emitCastAlignWarn(CheckerContext &C, unsigned SrcAlign,
@@ -367,6 +368,23 @@ void CapabilityAlignmentChecker::checkPostStmt(const BinaryOperator *BO,
   State = State->set<TrailingZerosMap>(ResVal.getAsSymbol(), Res);
   C.addTransition(State);
 }
+
+void CapabilityAlignmentChecker::checkDeadSymbols(SymbolReaper &SymReaper,
+                                                  CheckerContext &C) const {
+  ProgramStateRef State = C.getState();
+  TrailingZerosMapTy TZMap = State->get<TrailingZerosMap>();
+  bool Updated = false;
+  for (TrailingZerosMapTy::iterator I = TZMap.begin(), E = TZMap.end(); I != E;
+       ++I) {
+    if (SymReaper.isDead(I->first)) {
+      State = State->remove<TrailingZerosMap>(I->first);
+      Updated = true;
+    }
+  }
+  if (Updated)
+    C.addTransition(State);
+}
+
 namespace {
 
 bool hasCapability(const QualType OrigTy, ASTContext &Ctx) {

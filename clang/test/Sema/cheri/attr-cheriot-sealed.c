72ed78602d2885b8328fe4c0414204487cf85452
David Chisnall <davidchisnall@users.noreply.github.com>
2025-01-31 12:04:39 +0000

    [CHERI] Add a sealed type annotation (#109)
    This allows us to disallow dereferences and pointer indexing on
    sealed capabilities at compile time.
    
    Builtins that introspect capabilities can all work on a sealed or
    unsealed capability.
    
    Builtins that mutate capabilities are all disallowed on sealed
    capabilities, with the exception of clear-tag (which works on sealed or
    unsealed things).
    
    Reinterpret cast and C-style (explicit) casts are allowed.  Anyone who
    does an explicit unsafe cast should know what they're doing *anyway*
    this is another case of that.
    
    Implicit casts are also permitted to `void*`.  This avoids needing new
    overloads for things that treat `void*` as opaque values.  Casting back
    from `void*` to something useful already requires a dangerous explicit
    cast and you should know what you're doing if you do that.
    
    Also adds a __has_extension check for the sealed type qualifier.
    
    Co-authored-by: Owen Anderson <resistor@mac.com>

---
+++ clang/test/Sema/cheri/attr-cheriot-sealed.c
@@ -0,0 +1,54 @@
+// RUN: %clang_cc1 -triple riscv32cheriot -target-abi cheriot-baremetal -verify %s
+
+int * __sealed_capability test1(int * __sealed_capability a) {
+    return a; // no error
+}
+
+int test2(int * __sealed_capability a) {
+    return *a; // expected-error{{invalid operator applied to sealed type 'int * __sealed_capability'}}
+}
+
+int test3(int * __sealed_capability a) {
+    return a[0]; // expected-error{{invalid operator applied to sealed type 'int * __sealed_capability'}}
+}
+
+int * __sealed_capability test4(int * __sealed_capability a) {
+    return a + 1; // expected-error{{invalid operands to binary expression ('int * __sealed_capability' and 'int'}}
+}
+
+typedef struct { int b; } test_struct;
+
+int test5(test_struct * __sealed_capability a) {
+    return a->b; // expected-error{{cannot access members of sealed type 'test_struct * __sealed_capability'}}
+}
+
+int test6(int (* __sealed_capability myfun)()) {
+    return myfun(); // expected-error{{sealed type 'int (* __sealed_capability)(void)' cannot be called}}
+}
+
+int test7(int * __sealed_capability a) {
+    int *b = a; // expected-error{{converting sealed type 'int * __sealed_capability' to non-sealed type 'int *' without an explicit unsealing}}
+    return *b;
+}
+
+int test8(int * __sealed_capability a) {
+    int *b = (int*)a; // no error (*explicit* cast allowed)
+    return *b;
+}
+
+extern int deref(int *);
+extern void opaque(void*);
+
+int test9(int * __sealed_capability a) {
+    return deref(a); // expected-error{{converting sealed type 'int * __sealed_capability' to non-sealed type 'int *' without an explicit unsealing}}
+}
+
+extern int test9(int * __sealed_capability);
+int test10(int *a) {
+    return test9(a); // expected-error{{converting unsealed type 'int *' to sealed type 'int * __sealed_capability' without an explicit sealing}}
+}
+
+void test11(int * __sealed_capability x) {
+	opaque(x); // No error (cast to void permitted)
+}
+

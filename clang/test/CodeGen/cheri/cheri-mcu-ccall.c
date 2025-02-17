533ea1641ba3b720e394eb8fab9251281d50f214
Owen Anderson <resistor@mac.com>
2025-02-17 21:02:45 +1300

    [cheriot] Add cheriot spellings to calling convention-related attributes.

--- clang/test/CodeGen/cheri/cheri-mcu-ccall.c
+++ clang/test/CodeGen/cheri/cheri-mcu-ccall.c
@@ -1,11 +1,11 @@
 // RUN: %clang_cc1 %s -o - "-triple" "riscv32cheriot-unknown-cheriotrtos" "-emit-llvm" "-mframe-pointer=none" "-mcmodel=small" "-target-abi" "cheriot" "-Oz" "-Werror" "-cheri-compartment=example" | FileCheck %s
 
 // CHECK: define dso_local chericcallcce i32 @_Z5test2ii(i32 noundef %a0, i32 noundef %a1) local_unnamed_addr addrspace(200) #0
-__attribute__((cheri_compartment("example"))) int test2(int a0, int a1) {
+__attribute__((cheriot_compartment("example"))) int test2(int a0, int a1) {
   return a0 + a1;
 }
 
-__attribute__((cheri_compartment("other"))) int test6callee(int *a0, int *a1, int *a2, int *a3, int *a4, int *a5);
+__attribute__((cheriot_compartment("other"))) int test6callee(int *a0, int *a1, int *a2, int *a3, int *a4, int *a5);
 
 int testcall6() {
   static int stack_arg;

resistor/llvm-project-1

[CHERIoT] Support most operations on __sealed_capabilities in unevaluated contexts.
This is needed in order to support things like sizeof and decltype.
Casts to an from sealed types are still disallowed.

clang/test/Sema/cheri/attr-cheriot-sealed.c
@@ -46,3 +46,7 @@ extern int test9(int * __sealed_capability);
 int test10(int *a) {
     return test9(a); // expected-error{{converting unsealed type 'int *' to sealed type 'int * __sealed_capability' without an explicit sealing}}
 }
+
+int test11(int * __sealed_capability a) {
+    return sizeof(*a);
+}

CHERIoT-Platform/llvm-project

clang/test/Sema/cheri/attr-cheriot-sealed.c
@@ -52,3 +52,6 @@ void test11(int * __sealed_capability x) {
 	opaque(x); // No error (cast to void permitted)
 }
 
+int test12(int * __sealed_capability a) {
+    return sizeof(*a);
+}
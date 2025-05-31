ca9ae98aa794d7c749c80e04905cb05f83ac2c11
edoardo <ecmm@anche.no>
2025-05-31 14:57:20 +0200

    [CHERIoT] Add missing case for newly introduced `cheriot_sealed` attribute in type printer (#162)

--- clang/lib/AST/TypePrinter.cpp
+++ clang/lib/AST/TypePrinter.cpp
@@ -2132,6 +2132,9 @@ void TypePrinter::printAttributedAfter(const AttributedType *T,
   case attr::CHERIOTSharedObject:
     OS << "cheriot_shared_object";
     break;
+  case attr::CHERIoTSealedType:
+    OS << "cheriot_sealed";
+    break;
   case attr::FastCall: OS << "fastcall"; break;
   case attr::StdCall: OS << "stdcall"; break;
   case attr::ThisCall: OS << "thiscall"; break;

7829a8b744e82e43081efd9d7a6bb093c5238a1d
Jessica Clarke <jrtc27@jrtc27.com>
2025-07-12 00:45:05 +0100

    [ELF] Don't emit capreloc for non-preemptible undef weak symbols
    Currently we rely on cheri_init_globals or equivalent treating a base of
    0 as NULL. Historically when caprelocs encoded the absolute base this
    was relatively ok, if inefficient, but with caprelocs being implicitly
    relative this is ambiguous, as it could instead be a relocation
    referring to the start of the object, which we can no longer express. We
    could paper over that by checking the length is 0 too (breaking only if
    wanting to refer to a 0-byte region at the start of the object), but
    it's still inefficient and unnecessary. It also failed to account for
    the addend, always giving NULL, not NULL + addend. Since we have a true
    static link time constant, we should just write out the bits for a
    NULL-derived capability, just as the assembler does for .chericap const.
    
    This is also important downstream for Morello, and will be important
    here too, as when PCC bounds are correctly set in the relocations the
    base could legitimately be zero for a non-preemptible symbol that isn't
    itself zero. Morello therefore does not treat a base of 0 as special,
    and so is more broken for non-preemptible undef weak symbols, deriving
    real (if representable) capabilities, with an address that isn't even 0
    in the case of dynamically-linked binaries.

---
+++ lld/test/ELF/cheri/mips/undef-weak.s
@@ -0,0 +1,18 @@
+# REQUIRES: mips
+
+# RUN: llvm-mc -triple=mips64 -mcpu=cheri128 -mattr=+cheri128 -filetype=obj %s -o %t.128.o
+# RUN: ld.lld %t.128.o -o %t.128
+# RUN: llvm-readobj --cap-relocs %t.128 | FileCheck --check-prefix=RELOC %s
+# RUN: llvm-readobj -x .data %t.128 | FileCheck --check-prefix=HEX128 %s
+
+# RELOC: There is no __cap_relocs section in the file.
+
+# HEX128-LABEL: section '.data':
+# HEX128-NEXT:  [[#%x,]] 00000000 00000000 00000000 00000000
+# HEX128-NEXT:  [[#%x,]] 00000000 00000000 00000000 00000003
+
+.weak undef_weak
+
+.data
+.chericap undef_weak
+.chericap undef_weak + 3

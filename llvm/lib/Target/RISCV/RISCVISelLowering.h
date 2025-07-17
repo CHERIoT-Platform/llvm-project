2486434cb3a462edceaae1ee4e2e37bb449815bf
Owen Anderson <resistor@mac.com>
2025-07-17 20:51:47 +0800

    [CHERIOT] Use capability registers to store f64 values.
    This enables each f64 to be passed by value in a single cap register,
    rather than in pairs of integer registers.
    
    This required adding explicit type annotations to various places in the
    XCheri tblgen files, as the GPCR class can now hold values type c64 or
    f64, breaking type inference.

--- llvm/lib/Target/RISCV/RISCVISelLowering.h
+++ llvm/lib/Target/RISCV/RISCVISelLowering.h
@@ -507,7 +507,8 @@ private:
                             SelectionDAG &DAG) const;
   SDValue getTLSDescAddr(GlobalAddressSDNode *N, SelectionDAG &DAG) const;
 
-  SDValue lowerConstantFP(SDValue Op, SelectionDAG &DAG) const;
+  SDValue lowerConstantFP(SDValue Op, SelectionDAG &DAG,
+                          const RISCVSubtarget &Subtarget) const;
   SDValue lowerGlobalAddress(SDValue Op, SelectionDAG &DAG) const;
   SDValue lowerBlockAddress(SDValue Op, SelectionDAG &DAG) const;
   SDValue lowerConstantPool(SDValue Op, SelectionDAG &DAG) const;

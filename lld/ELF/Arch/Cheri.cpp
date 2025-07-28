5c6a26127896dcba38b0bdcf655098d4bf371447
Jessica Clarke <jrtc27@jrtc27.com>
2025-07-28 17:37:05 +0100

    [NFC][ELF][CHERI] Fix relocation name in comments and add TODO
    These are the actual target relocations and not the RelExpr.

--- lld/ELF/Arch/Cheri.cpp
+++ lld/ELF/Arch/Cheri.cpp
@@ -86,8 +86,9 @@ SymbolAndOffset SymbolAndOffset::fromSectionWithOffset(
     }
   }
   // When using the legacy __cap_relocs style (where clang emits __cap_relocs
-  // instead of R_CHERI_CAPABILITY) the local symbols might not exist so we
+  // instead of R_*_CHERI_CAPABILITY) the local symbols might not exist so we
   // may have fall back to the section.
+  // TODO: Revisit this now we no longer support __cap_relocs input.
   if (!fallbackResult) {
     // worst case we fall back to the section + offset
     // Don't warn if the relocation is against an anonymous string constant
@@ -944,7 +945,8 @@ void addCapabilityRelocation(
   if (sym)
     assert(sym->isPreemptible || !sym->isUndefWeak());
 
-  // Emit either the legacy __cap_relocs section or a R_CHERI_CAPABILITY reloc
+  // Emit either the legacy __cap_relocs section or a R_*_CHERI_CAPABILITY
+  // reloc
   // For local symbols we can also emit the untagged capability bits and
   // instruct csu/rtld to run CBuildCap
   if ((!sym || !sym->isPreemptible) && !needTrampoline) {
@@ -963,7 +965,7 @@ void addCapabilityRelocation(
     printLocationMessage(diag, *sec, *sym, offset);
   }
 
-  // We don't use a R_MIPS_CHERI_CAPABILITY relocation for the input but
+  // We don't use a R_*_CHERI_CAPABILITY relocation for the input but
   // instead need to use an absolute pointer size relocation to write
   // the offset addend
   if (!dynRelSec)

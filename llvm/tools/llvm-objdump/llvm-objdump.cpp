0a5fbf8d32b7edda172dbfecd9d4b08d6043e928
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-12 19:17:51 +0100

    [llvm-objdump][llvm-readobj] Decode code caprelocs permissions
    For non-c18n runtimes this is the same as a plain function capreloc, but
    for c18n runtimes this marks capabilities that must not be wrapped in a
    trampoline as they must point directly to the code in question.

--- llvm/tools/llvm-objdump/llvm-objdump.cpp
+++ llvm/tools/llvm-objdump/llvm-objdump.cpp
@@ -2732,6 +2732,7 @@ printELFCapRelocations(const ELFObjectFile<ELFT> *Obj) {
     const uint64_t Function = UINT64_C(1) << ((sizeof(TargetUint) * 8) - 1);
     const uint64_t Constant = UINT64_C(1) << ((sizeof(TargetUint) * 8) - 2);
     const uint64_t Indirect = UINT64_C(1) << ((sizeof(TargetUint) * 8) - 3);
+    const uint64_t Code = UINT64_C(1) << ((sizeof(TargetUint) * 8) - 4);
     StringRef PermStr;
     switch (Perms) {
     case 0:
@@ -2745,6 +2746,9 @@ printELFCapRelocations(const ELFObjectFile<ELFT> *Obj) {
       break;
     case Function | Indirect:
       PermStr = " (GNU Indirect Function)";
+      break;
+    case Function | Code:
+      PermStr = " (Code)";
       break;
     default:
       PermStr = " (Unknown)";

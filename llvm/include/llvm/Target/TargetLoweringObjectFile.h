2da350a7102c4dcce771ef4751136c1a5120e1e5
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-20 02:20:37 +0100

    [NFC][Target][AsmPrinter] Introduce and use lowerCheriCodeReference
    This will allow targets to create a target-specific MCExpr for referring
    to code, bypassing any use of trampolines in the presence of a c18n
    runtime.

--- llvm/include/llvm/Target/TargetLoweringObjectFile.h
+++ llvm/include/llvm/Target/TargetLoweringObjectFile.h
@@ -215,6 +215,13 @@ public:
     return nullptr;
   }
 
+  /// Return an MCExpr to use for a reference to the specified symbol with the
+  /// specified offset, where the symbol is a function symbol but we do not
+  /// want any interposition, as this reference is for a specific instruction
+  /// within the function (e.g. a BlockAddress).
+  virtual const MCExpr *lowerCheriCodeReference(const MCSymbol *Sym,
+                                                const MCExpr *Addend) const;
+
   /// Target supports a PC-relative relocation that references the PLT of a
   /// function.
   bool hasPLTPCRelative() const { return PLTPCRelativeSpecifier; }

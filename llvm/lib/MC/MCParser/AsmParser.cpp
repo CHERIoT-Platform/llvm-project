8afd3eb86d905276558738dd68bb6781a242e748
Jessica Clarke <jrtc27@jrtc27.com>
2025-08-20 02:56:39 +0100

    [RISCV] Add MC support for .chericap %code(expr)

--- llvm/lib/MC/MCParser/AsmParser.cpp
+++ llvm/lib/MC/MCParser/AsmParser.cpp
@@ -5771,7 +5771,7 @@ bool AsmParser::parseDirectiveCheriCap(SMLoc DirectiveLoc) {
   if (!getTargetParser().isCheri())
     return Error(DirectiveLoc, "'.chericap' requires CHERI");
 
-  if (parseExpression(Expr))
+  if (getTargetParser().parseDataExpr(Expr))
     return true;
 
   int64_t Value;

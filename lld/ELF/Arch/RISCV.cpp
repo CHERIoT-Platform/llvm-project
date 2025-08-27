151d7790330a558b06e7d9572023509a5c9994c7
Dapeng Gao <dapeng.gao@cl.cam.ac.uk>
2025-08-27 17:48:07 +0100

    [ELF][RISCV] Handle R_RISCV_FUNC_RELATIVE in getImplicitAddend
    Otherwise linking with --apply-dynamic-relocs (as is done by U-Boot, or
    due to -z rel which we don't support) when --check-dynamic-relocs is
    enabled (which is the default in assertions builds) will give an
    internal linker error when such a relocation is emitted.
    
    Fixes: 2d52aa89a7be ("[ELF][RISCV] Support R_RISCV_CHERI_CAPABILITY_CODE and R_RISCV_FUNC_RELATIVE")

--- lld/ELF/Arch/RISCV.cpp
+++ lld/ELF/Arch/RISCV.cpp
@@ -225,6 +225,7 @@ int64_t RISCV::getImplicitAddend(const uint8_t *buf, RelType type) const {
     return read64le(buf);
   case R_RISCV_RELATIVE:
   case R_RISCV_IRELATIVE:
+  case R_RISCV_FUNC_RELATIVE:
     return ctx.arg.is64 ? read64le(buf) : read32le(buf);
   case R_RISCV_NONE:
   case R_RISCV_JUMP_SLOT:

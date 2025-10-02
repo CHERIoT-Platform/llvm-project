254fdfdbffa28ee13d093a0b86a924c6130f8c25
Owen Anderson <resistor@mac.com>
2025-10-02 11:32:18 +0900

    [CHERIoT] Enable the B extension for cheriot-ibex and cheriot-kudu.

--- clang/test/Driver/cheri/cheriot.c
+++ clang/test/Driver/cheri/cheriot.c
@@ -1,9 +1,17 @@
-// RUN: %clang -target riscv32cheriot-unknown-unknown -mcpu=cheriot -### -c %s 2>&1 | FileCheck %s -check-prefixes BAREMETAL,ALL
-// RUN: %clang -target riscv32cheriot-unknown-unknown -mabi=cheriot-baremetal -### -c %s 2>&1 | FileCheck %s -check-prefixes BAREMETAL,ALL
-// RUN: %clang -target riscv32cheriot-unknown-cheriotrtos -### -c %s 2>&1 | FileCheck %s -check-prefixes RTOS,ALL
+// RUN: %clang -target riscv32cheriot-unknown-unknown -mcpu=cheriot -### -c %s 2>&1 | FileCheck %s -check-prefixes BAREMETAL,CHERIOT,ALL
+// RUN: %clang -target riscv32cheriot-unknown-unknown -mabi=cheriot-baremetal -### -c %s 2>&1 | FileCheck %s -check-prefixes BAREMETAL,CHERIOT,ALL
+// RUN: %clang -target riscv32cheriot-unknown-cheriotrtos -### -c %s 2>&1 | FileCheck %s -check-prefixes RTOS,CHERIOT,ALL
+// RUN: %clang -target riscv32cheriot-unknown-unknown -mcpu=cheriot-ibex -### -c %s 2>&1 | FileCheck %s -check-prefixes IBEX,ALL
+// RUN: %clang -target riscv32cheriot-unknown-unknown -mcpu=cheriot-kudu -### -c %s 2>&1 | FileCheck %s -check-prefixes KUDU,ALL
 
+// CHERIOT: "-target-cpu" "cheriot"
+// IBEX: "-target-cpu" "cheriot-ibex"
+// KUDU: "-target-cpu" "cheriot-kudu"
 
-// ALL: "-target-cpu" "cheriot"
+// CHERIOT-NOT: "-target-feature" "+b"
+// IBEX: "-target-feature" "+b"
+// KUDU: "-target-feature" "+b"
+
 // ALL: "-target-feature" "+xcheriot"
 
 // BAREMETAL: "-target-abi" "cheriot-baremetal"

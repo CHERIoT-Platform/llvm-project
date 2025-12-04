115535fde4301c4070e3a651dc5ffb24fd307aea
Jessica Clarke <jrtc27@jrtc27.com>
2025-12-04 15:52:52 +0000

    [ELF] Sort __tgot_cap_relocs like __cap_relocs
    It's also a REL*-like section that's PROGBITS.

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -767,9 +767,11 @@ unsigned elf::getSectionRank(Ctx &ctx, OutputSection &osec) {
     // Make PROGBITS sections (e.g .rodata .eh_frame) closer to .text to
     // alleviate relocation overflow pressure. Large special sections such as
     // .dynstr and .dynsym can be away from .text.
+    // Treat __cap_relocs like REL* even though it's PROGBITS.
     else if (osec.type != SHT_PROGBITS)
       rank |= 4;
-    else if (osec.type == SHT_PROGBITS && osec.name != "__cap_relocs")
+    else if (osec.type == SHT_PROGBITS && osec.name != "__cap_relocs" &&
+             osec.name != "__tgot_cap_relocs")
       rank |= RF_RODATA;
   } else if (isExec) {
     // Place readonly .ltext before .lrodata and writable .ltext after .lbss to

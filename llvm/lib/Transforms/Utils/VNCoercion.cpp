e3e1bf0b6eec2b98400c91a515b6481e17fb1661
Owen Anderson <resistor@mac.com>
2026-02-18 09:39:53 +0100

    [CHERI][GVN] Fix a GVN crash when coercing CHERI nullptr to integer 0.
    This is the reverse direction of the previous fix. Also, change the
    predicates for both fixes to be CHERI-specific, as it's not clear
    that the null-equivalence is meant to apply to non-CHERI non-integral
    pointers.

--- llvm/lib/Transforms/Utils/VNCoercion.cpp
+++ llvm/lib/Transforms/Utils/VNCoercion.cpp
@@ -128,7 +128,7 @@ Value *coerceAvailableValueToLoadType(Value *StoredVal, Type *LoadedTy,
         StoredVal = Helper.CreatePtrToInt(StoredVal, StoredValTy);
       }
 
-      if (DL.isNonIntegralPointerType(LoadedTy)) {
+      if (DL.isFatPointer(LoadedTy)) {
         ConstantInt *CI = dyn_cast<ConstantInt>(StoredVal);
         assert(CI && CI->isZero());
 
@@ -402,7 +402,12 @@ static Value *getStoreValueForLoadHelper(Value *SrcVal, unsigned Offset,
   // Compute which bits of the stored value are being used by the load.  Convert
   // to an integer type to start with.
   // FIXME: Extracting bits works differently for CHERI capabilities
-  assert(!DL.isFatPointer(SrcVal->getType()));
+  if (DL.isFatPointer(SrcVal->getType())) {
+    ConstantPointerNull *CP = dyn_cast<ConstantPointerNull>(SrcVal);
+    assert(CP);
+    return Constant::getNullValue(LoadTy);
+  }
+
   if (SrcVal->getType()->isPtrOrPtrVectorTy())
     SrcVal =
         Builder.CreatePtrToInt(SrcVal, DL.getIntPtrType(SrcVal->getType()));

21cac10c29893fe914eb2e3b1c80190de5a5f065
Owen Anderson <resistor@mac.com>
2026-08-10 10:22:01 +0200

    [CHERI][clang] Defer checking of CHERI compartment annotation name mismatches until CodeGen.
    The reason for this is to ensure that clang-tidy works even in environments where the CLI
    compartment name is not set properly. This is fairly common, since clang-tidy is often run
    without recreating the full compilation command.

--- clang/test/Sema/cheri/cheri-mcu-compartment-warn-wrong-compartment.c
+++ clang/test/Sema/cheri/cheri-mcu-compartment-warn-wrong-compartment.c
@@ -8,17 +8,3 @@ int add(int a, int b) // wrong-compartment-error{{CHERI libcall exported from co
 {
 	return a+b;
 }
-
-__attribute__((cheriot_compartment("example")))
-int shouldBePrototype(void) // libcall-error{{CHERI compartment entry declared for compartment 'example' but implemented in '' (provided with -cheri-compartment=)}} wrong-compartment-error{{CHERI compartment entry declared for compartment 'example' but implemented in 'wrong' (provided with -cheri-compartment=)}}
-{
-	return 1;
-}
-
-__attribute__((cheriot_libcall))
-inline int inlineDefinition(int a, int b)
-// wrong-compartment-error@-1{{CHERI libcall should have a prototype in a header with a matching libcall annotation}}
-// libcall-error@-2{{CHERI libcall should have a prototype in a header with a matching libcall annotation}}
-{
-	return a+b;
-}
